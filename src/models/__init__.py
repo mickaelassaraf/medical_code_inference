5cf8dd8e61ee73e32b17579b73a6bbf473c49956
kunschg <guillaume.kunsch@qantev.com>
2023-09-12 17:07:49 +0000

    models refacto

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,11 +1,7 @@
 from src.models.base_model import BaseModel
 from src.models.laat import LAAT
-from src.models.mullenbach_models import (
-    CAML,
-    MullenbachBaseModel,
-    VanillaConv,
-    VanillaRNN,
-)
+from src.models.mullenbach_models import (CAML, MullenbachBaseModel,
+                                          VanillaConv, VanillaRNN)
 from src.models.multi_res_cnn import MultiResCNN
 from src.models.plm_cpt_hierarchical_embedding import PLMCPT_HIERARCHICAL
 from src.models.plm_icd import PLMICD
